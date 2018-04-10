f3833535e06002acc7de6a7bd61de389e41a2b88
tictacmenthe <ugurcan.ekren@telecom-sudparis.eu>
2018-04-10 17:36:08 +0200

    hack anti-segfault en fin de programme

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-#include "RPLidar/include/rplidar.h"
+#include <zconf.h>
+#include "RPLidar/sdk/include/rplidar.h"
 
 using namespace rp::standalone::rplidar;
 int main()
 {
 
-    RPlidarDriver* driver=RPlidarDriver::CreateDriver();                    //L'interface avec le LiDAR
+    RPlidarDriver* driver=RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);                    //L'interface avec le LiDAR
 
     if(driver->connect("/dev/ttyUSB0", 115200) == RESULT_OK) {               //Connection au LiDAR sur le port serie /dev/ttyUSB0 a 115200 bauds
         std::cout<<"rplidar: lidar connecte"<<std::endl;
         rplidar_response_device_health_t health_response;
         driver->getHealth(health_response);
         std::cout<<"status: "<<health_response.status<<" error code: "<<health_response.error_code<<std::endl;
+
+        driver->setMotorPWM(500);
+        //usleep(5000000);
+        driver->stopMotor();
         driver->disconnect();
     }
     else{
         std::cout<<"Erreur rplidar: connection echouee"<<std::endl;
     }
+
+    std::cout<<driver<<std::endl;
     RPlidarDriver::DisposeDriver(driver);
+
+    std::cout<<"plsnosegfault"<<std::endl;
+    return 0;
 }
