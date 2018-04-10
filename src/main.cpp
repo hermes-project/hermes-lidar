48dd4bad82a36b9707d0116c9288c108231b8a91
tictacmenthe <ugurcan.ekren@telecom-sudparis.eu>
2018-04-10 13:06:05 +0200

    ajout de -pthread dans les flags du compilateur, pour des fonctions crades de threading du sdk

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,18 @@
 using namespace rp::standalone::rplidar;
 int main()
 {
-    RPlidarDriver* driver=RPlidarDriver::CreateDriver();
-    driver->connect()
 
+    RPlidarDriver* driver=RPlidarDriver::CreateDriver();                    //L'interface avec le LiDAR
 
+    if(driver->connect("/dev/ttyUSB0", 115200) == RESULT_OK) {               //Connection au LiDAR sur le port serie /dev/ttyUSB0 a 115200 bauds
+        std::cout<<"rplidar: lidar connecte"<<std::endl;
+        rplidar_response_device_health_t health_response;
+        driver->getHealth(health_response);
+        std::cout<<"status: "<<health_response.status<<" error code: "<<health_response.error_code<<std::endl;
+        driver->disconnect();
+    }
+    else{
+        std::cout<<"Erreur rplidar: connection echouee"<<std::endl;
+    }
     RPlidarDriver::DisposeDriver(driver);
 }
